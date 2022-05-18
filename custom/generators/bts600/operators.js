b437346cd7b0d301343bfeaf9fafb01ff44114d4
Elias Barbers <Elias.Barbers@isea.rwth-aachen.de>
2022-05-18 16:51:05 +0200

    blocks for operators etc.

---
+++ custom/generators/bts600/operators.js
@@ -0,0 +1,23 @@
+Blockly.BTS600['set'] = function (block) {
+  return '\t\t\t\t\t\t\n';
+};
+
+Blockly.BTS600['beg'] = function (block) {
+  return '\t\t\t\t\t\t\n';
+};
+
+Blockly.BTS600['cyc'] = function (block) {
+  return '\t\t\t\t\t\t\n';
+};
+
+Blockly.BTS600['cha'] = function (block) {
+  return '\t\t\t\t\t\t\n';
+};
+
+Blockly.BTS600['dch'] = function (block) {
+  return '\t\t\t\t\t\t\n';
+};
+
+Blockly.BTS600['sto'] = function (block) {
+  return '\t\t' + 'STO' + '\t\t\t\t\n';
+};
