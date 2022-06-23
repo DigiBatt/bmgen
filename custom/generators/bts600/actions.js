405390983708391eff8ab43c7d177152438b2e39
Elias Barbers <Elias.Barbers@isea.rwth-aachen.de>
2022-06-23 10:42:38 +0200

    remove unused files

--- custom/generators/bts600/actions.js
+++
@@ -1,8 +0,0 @@
-Blockly.BTS600['goto'] = function (block) {
-    return new BTSGoto(label);
-}
-
-Blockly.BTS600['err'] = function (block) {
-    const errnum = block.getFieldValue('ERRNUM');
-    return new BTSError(errnum);
-};
--- custom/generators/bts600/channels.js
+++
@@ -1,4 +0,0 @@
-Blockly.BTS600['channel'] = function (block) {
-    const channel = block.getFieldValue('CHANNEL');
-    return new BTSVariable(channel);
-};
