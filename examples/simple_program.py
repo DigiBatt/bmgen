f4d4f4dabac4018f552c8eb477673a81a59050e4
Elias Barbers <Elias.Barbers@isea.rwth-aachen.de>
2023-08-31 10:15:36 +0200

    fix limit error in example program

--- examples/simple_program.py
+++ examples/simple_program.py
@@ -3,7 +3,7 @@ from bmgen.function import *
 from bmgen.channel import *
 
 # set safety limits
-limit(V < 4.2, error(1))
+limit(V > 4.2, error(1))
 limit(V < 2.5, error(1))
 limit(I > 5, error(1))
 limit(I < -5, error(1))
