ba740d4bb5706cfb4964771fc381ab15b8e07a7f
Elias Barbers <Elias.Barbers@isea.rwth-aachen.de>
2023-09-12 22:09:01 +0200

    add more battery parameters

--- src/bmgen/targets/basytec/battery.py
+++ src/bmgen/targets/basytec/battery.py
@@ -1,5 +1,7 @@
 from bmgen.targets.basytec.ast import BasytecValue, BasytecUnit
 
 nominalCapacity = BasytecValue(1, BasytecUnit("CN"))
+maxVoltage = BasytecValue(1, BasytecUnit("UBatMax"))
+minVoltage = BasytecValue(1, BasytecUnit("UBatMin"))
 eocVoltage = BasytecValue(1, BasytecUnit("UBatCh"))
 eodVoltage = BasytecValue(1, BasytecUnit("UBatDch"))
--- src/bmgen/targets/bm/battery.py
+++ src/bmgen/targets/bm/battery.py
@@ -1,5 +1,12 @@
 from bmgen.targets.bm.ast import BMVariable
 
 nominalCapacity = BMVariable("ACn1")
+maxVoltage = BMVariable("UMax")
+minVoltage = BMVariable("UNom")
 eocVoltage = BMVariable("UGas")
 eodVoltage = BMVariable("CutOff")
+contChargeCurrent = BMVariable("ChargeF")
+contDischargeCurrent = BMVariable("ICrank")
+nominalCurrent = BMVariable("INom")
+internalResistance = BMVariable("Rin")
+energyDensity = BMVariable("EDensity")
