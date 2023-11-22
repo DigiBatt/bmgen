f05d148a2b6ec549442c10029e1c344e8ced4320
Elias Barbers <Elias.Barbers@isea.rwth-aachen.de>
2023-11-22 18:37:33 +0100

    Basytec: additional battery parameters

--- src/bmgen/targets/basytec/battery.py
+++ src/bmgen/targets/basytec/battery.py
@@ -3,9 +3,14 @@ from bmgen.battery import CyclerBattery
 
 battery = CyclerBattery(
     nominalCapacity=BasytecValueLiteral(1, BasytecUnit("CN")),
-    maxVoltage=BasytecValueLiteral(1, BasytecUnit("UBatMax")),
     minVoltage=BasytecValueLiteral(1, BasytecUnit("UBatMin")),
-    eocVoltage=BasytecValueLiteral(1, BasytecUnit("UBatCh")),
+    maxVoltage=BasytecValueLiteral(1, BasytecUnit("UBatMax")),
+    nominalVoltage=BasytecValueLiteral(1, BasytecUnit("UN")),
     eodVoltage=BasytecValueLiteral(1, BasytecUnit("UBatDch")),
+    eocVoltage=BasytecValueLiteral(1, BasytecUnit("UBatCh")),
+    contChargeCurrent=BasytecValueLiteral(1, BasytecUnit("IBatCh")),
+    peakChargeCurrent=BasytecValueLiteral(1, BasytecUnit("IBatMax")),
+    contDischargeCurrent=BasytecValueLiteral(1, BasytecUnit("IBatDch")),
+    peakDischargeCurrent=BasytecValueLiteral(1, BasytecUnit("IBatMin")),
     oneC=BasytecValueLiteral(1, BasytecUnit("CA")),
 )
